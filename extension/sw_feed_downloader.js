7c1a72ac131325b0d90d21d4ab7fef0c5eb56520
lucasmorita <lucasmnova@gmail.com>
2022-03-20 01:15:45 -0300

    Refactoring code
    - Change logs to DEBUG level

--- extension/sw_feed_downloader.js
+++ extension/sw_feed_downloader.js
@@ -1,11 +1,9 @@
-var posts = [];
 function getTab() {
     return chrome.tabs.query({ active: true, currentWindow: true });
 }
 
 function isFullResolutionVideoLink(details) {
-    let isFullResolution = details.url.includes('?tag');
-    return isFullResolution;
+    return details.url.includes('?tag');
 }
 
 function postToDownload(url) {
@@ -13,59 +11,77 @@ function postToDownload(url) {
     chrome.runtime.sendMessage({ initiator: document.URL, target: url});
 }
 
-chrome.runtime.onMessage.addListener((req, sender, _) => {
-    console.debug(`${req.initiator} initiated request to download for ${req.target}!`);
-    posts[req.initiator] = req.target;
-})
-
-function onBeforeRequestCallback(details) {
-    let isHome = isHomePage()
-    if (isFullResolutionVideoLink(details) && isHome) {
-        console.debug(`getting main video: ${details.url}`);
-        getTab().then((tabs) => {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: postToDownload,
-                args: [details.url]
-            });
-        });
-    }
-}
-
-chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestCallback, {
-    urls: ["https://video.twimg.com/ext_tw_video/*.m3u8*"],
-});
-
-async function download(url, currentTab) {
-    console.debug(`requesting video for url ${url}`)
-    let server = "https://d518-179-113-155-6.ngrok.io/";
-    await fetch(server+'videos', {
-        body: JSON.stringify({
-            videoUrl: url,
-        }),
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "http://localhost:5000",
-        },
-        method: "POST",
-    });
-}
 
 async function isHomePage() {
     let tabs = await getTab();
-
     console.debug(`comparing ${tabs[0].url} with homepage`)
     if (tabs[0].url.startsWith("https://twitter.com/home"))
         return true;
     return false;
 }
 
+function onBeforeRequestCallback(details) {
+    chrome.storage.local.get('actualPage', result => {
+        console.debug(`actualPage ${JSON.stringify(result)}`)
+        if (result) {
+            if (isFullResolutionVideoLink(details) && isHomePage()) {
+                console.debug(`getting main video: ${details.url}`);
+                getTab().then((tabs) => {
+                    chrome.scripting.executeScript({
+                        target: { tabId: tabs[0].id },
+                        func: postToDownload,
+                        args: [details.url]
+                    });
+                });
+            }
+            chrome.storage.local.remove('actualPage');
+        }
+    })
+    
+}
+
+function webNavigationListener(details) {
+    console.debug(`Setting actualPage to ${details.url}`);
+    chrome.storage.local.set({actualPage: details.url});
+}
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(webNavigationListener, {
+    pathContains: 'status'
+});
+
+
+
+async function requestDownload(url, currentTab) {
+    console.debug(`requesting video for url ${url}`)
+    let server = "https://d518-179-113-155-6.ngrok.io/";
+    await fetch(server + 'videos', {
+        body: JSON.stringify({
+            videoUrl: url,
+        }),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        method: "POST",
+    });
+}
+
+// event listeners
 chrome.action.onClicked.addListener((tab) => {
     if (tab.url.startsWith('https://twitter.com/home')) {
         console.debug('cannot download at home page!');
         return;
     }
     console.debug(`initiating download for [${posts[tab.url]}]`);
-    download(posts[tab.url].split('?')[0], tab);
+    requestDownload(posts[tab.url].split('?')[0], tab);
+});
+
+chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestCallback, {
+    urls: ["https://video.twimg.com/ext_tw_video/*.m3u8?tag*"],
+});
+
+chrome.runtime.onMessage.addListener((req, sender, _) => {
+    console.debug(`${req.initiator} initiated request to download for ${req.target}!`);
+    posts[req.initiator] = req.target;
 });
