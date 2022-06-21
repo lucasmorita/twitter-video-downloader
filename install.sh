9648f2dc9696da9b3a7b755dd410a381df5d571d
luiznux <luiztagli@hotmail.com>
2022-06-20 23:17:37 -0300

    [feature] - add new readme and install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dir=$(pwd)
+
+setup_extension() {
+    cd $dir/extension \
+        && npm install && npm run build \
+        && echo "Installation DONE!"
+}
+
+setup_server() {
+    cd $dir/server \
+        && docker build . -t twitter-vid-downloader \
+        && docker run -v {dir_with_videos}:/usr/src/app/video -p 5000:5000 twitter-vid-downloader \
+        && echo "Docker container Done!"
+}
+
+#### MAIN ####
+etup_extension
+setup_server
