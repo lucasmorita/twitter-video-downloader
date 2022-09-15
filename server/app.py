fec45956deac626a35322a4fb06a403a6e39c835
lucasmorita <lucasmnova@gmail.com>
2022-09-15 09:44:54 -0300

    Refactor app.py, add pyinstaller

--- server/app.py
+++ server/app.py
@@ -1,49 +1,50 @@
 import json
 
-from flask import Flask, send_from_directory, jsonify, request, url_for, make_response
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from uuid import uuid4
+from pathlib import Path
 import os
-
-from werkzeug.utils import redirect, send_file
+import platform
 
 app = Flask(__name__)
 CORS(app)
-app.config['TWITTER_VIDEOS'] = os.path.join(os.path.dirname(__file__), './videos')
+home = Path.home()
+if platform.system() == 'Windows':
+    videopath = os.path.join(home, 'Videos')
+    app.config['TWITTER_VIDEOS'] = videopath
+elif platform.system() == 'Linux':
+    videopath = os.path.join(home, 'videos')
+    app.config['TWITTER_VIDEOS'] = videopath
+
+
+def get_twitter_videos_path(filename):
+    return os.path.join(app.config['TWITTER_VIDEOS'], filename)
 
 
 def convert(url):
+    """
+    Generate a file using ffmpeg, with a random name
+    """
     randomname = str(uuid4())
     sanitizedurl = url.split("?")[0]
     print(f'convert {sanitizedurl} as {randomname}')
-    os.system(f'ffmpeg -i {sanitizedurl} -c copy -bsf:a aac_adtstoasc -hide_banner -loglevel error ./videos/{randomname}.mp4')
+    file = get_twitter_videos_path(randomname + ".mp4")
+    os.system(
+        f'ffmpeg -i {sanitizedurl} -c copy -hide_banner -loglevel error "{file}"')
     return randomname
 
 
-@app.route('/videos', methods=['POST', 'GET'])
+@app.route('/videos', methods=['POST'])
 @cross_origin()
 def videos():
     if request.method == 'POST':
         requestjson = request.data.decode('utf8').replace("'", '"')
         data = json.loads(requestjson)
         name = convert(data['videoUrl'])
-        return {"filename": name}
-    if request.method == 'GET':
-        data = {"videos": os.listdir(os.path.join('.', 'videos'))}
-        return jsonify(data), 200
+        return jsonify({"filename": name})
     return "not possible", 400
 
 
-@app.route('/downloads', methods=['GET'])
-@cross_origin()
-def downloads():
-    if request.method == 'GET':
-        res = make_response(send_from_directory(app.config['TWITTER_VIDEOS'],
-                                                path=request.args.get('filename') + '.mp4',
-                                                as_attachment=True))
-        return res
-
-
 if __name__ == '__main__':
-    print(os.listdir(os.path.join('.', 'videos')))
     app.run(debug=True, host="0.0.0.0", port=5000)
