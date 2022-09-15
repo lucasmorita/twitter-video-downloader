6796f6da4500fbca12e8ad887a731024ddfb350c
lucasmorita <lucasmnova@gmail.com>
2022-09-15 10:02:08 -0300

    Copy popup files and manifest json when building

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/popup.html' },
+        { from: 'src/manifest.json' },
+        { from: 'src/popup.css' }  
+      ]
+    })
+  ],
   entry: {
     background: path.resolve(__dirname, 'src', 'typescript', 'background.ts'),
     popup: path.resolve(__dirname, 'src', 'typescript', 'popup.ts')
   },
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'build'),
     filename: '[name].js'
   },
   module: {
